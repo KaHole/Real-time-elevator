4afc300aad31bae268837d7100809685dbed2432
kahole <kristian@hole.priv.no>
2018-02-13 19:56:23 +0100

    sets up the two sockets in their respective loops

--- src/discover.erl
+++ src/discover.erl
@@ -1,21 +1,33 @@
 -module(discover).
 -export([start/0]).
 
-
 start() ->
     % binary | list   ... list might be really useful!
     % {ok, Socket} = gen_udp:open(8889, [binary, {broadcast, true}]),
-    {ok, Socket} = gen_udp:open(8889, [binary, {active, true}]),
-    listen(Socket).
 
+    spawn(fun() -> listen() end),
+    spawn(fun() -> broadcast() end).
 
 % Bruk   192.168.1.255:8889  for broadcast sending!
 
 
+broadcast() ->
+    {ok, Socket} = gen_udp:open(8777, [binary, {active, true}, {broadcast, true}]),
+    broadcast(Socket).
+
+
 broadcast(Socket) ->
     io:format("~p~n", [node(self())]),
-    gen_udp:send(Socket, {192,168,1,255}, 8889, node(self())).
+    % gen_udp:send(Socket, {192, 168, 1, 255}, 8889, node(self())).
+    gen_udp:send(Socket, {192, 168, 1, 255}, 8889, "god dag\n"),
 
+    timer:sleep(1000),
+    broadcast(Socket).
+
+
+listen() ->
+    {ok, Socket} = gen_udp:open(8889, [binary, {active, true}]),
+    listen(Socket).
 
 listen(Socket) ->
     receive
