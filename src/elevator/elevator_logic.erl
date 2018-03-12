0f5057eb239a3925e2e9d38d21567ab5219226cb
hola <havard.ola.eggen@gmail.com>
2018-03-12 20:00:33 +0100

    Control over the elevator from cab. Still crued, but getting close.

--- src/elevator/elevator_logic.erl
+++ src/elevator/elevator_logic.erl
@@ -5,10 +5,14 @@
 
 start() ->
     {_, Pid} = elevator_interface:start(),
-    Floor = elevator_interface:get_floor_sensor_state(Pid),
     io:fwrite("~p~n", [Pid]),
-    State = #elevator{floor=Floor},
-    init(Pid, State).
+    init(Pid, #elevator{floor=elevator_interface:get_floor_sensor_state(Pid)}),
+    
+    Cab_server_pid = spawn(fun() -> cab_server(
+            Pid, 
+            #elevator{floor=elevator_interface:get_floor_sensor_state(Pid)}) 
+        end),
+    io:fwrite("~p~n", [Cab_server_pid]).
 
 init(Pid, #elevator{floor=between_floors}) ->
     elevator_interface:set_motor_direction(Pid, up),
@@ -20,4 +24,51 @@ init(Pid, _) ->
     elevator_interface:set_motor_direction(Pid, stop),
     ok.
 
+cab_server(Pid, State) ->
+    receive
+        {_, Floor} -> cab_server(Pid, cab_call(Pid, State, Floor));
+        _ -> io:format("njet~n"),
+                cab_server(Pid, State)
+    end.
 
+cab_call(Pid, State, Floor) ->
+    elevator_interface:set_door_open_light(Pid, off),
+    elevator_interface:set_order_button_light(Pid, cab, Floor, on),
+    elevator_interface:set_floor_indicator(Pid, State#elevator.floor),
+    
+    ToFloor = State#elevator.floor - Floor,
+    Direction = if 
+        ToFloor > 0 -> down;
+        ToFloor < 0 -> up;
+        true -> stop
+    end,
+    move(Pid, 
+        State#elevator{behaviour=idle, direction=Direction},
+        Floor).
+
+move(Pid, #elevator{direction=stop} = State, NewFloor) ->
+    elevator_interface:set_motor_direction(Pid, State#elevator.direction),
+    elevator_interface:set_door_open_light(Pid, on),
+    elevator_interface:set_order_button_light(Pid, cab, State#elevator.floor, off),
+    elevator_interface:set_floor_indicator(Pid, State#elevator.floor),
+    State;
+
+move(Pid, #elevator{behaviour=moving} = State, NewFloor) ->
+    Floor = elevator_interface:get_floor_sensor_state(Pid),
+    timer:sleep(100),
+    elevator_interface:set_floor_indicator(Pid, State#elevator.floor),
+    if 
+        Floor == NewFloor -> move(Pid, 
+                State#elevator{behaviour=idle, direction=stop, floor=Floor}, NewFloor);
+        Floor /= between_floors -> move(Pid, State#elevator{floor=Floor}, NewFloor);
+        true -> move(Pid, State, NewFloor)
+    end;
+
+move(Pid, State, NewFloor) ->
+    elevator_interface:set_motor_direction(Pid, State#elevator.direction),
+    timer:sleep(500),
+    move(Pid, State#elevator{behaviour=moving}, NewFloor).
+    
+
+
+    
