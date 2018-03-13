b45d1a6f04e434eda6f67c4cd0857a111783fa6d
kahole <kristian@hole.priv.no>
2018-03-13 21:06:24 +0100

    added simulator executable and script for starting 4 elevator

---
+++ scripts/mac_4.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+nodes=( one two three four )
+port=15657
+
+for node in "${nodes[@]}"
+do
+    osascript -e "tell app \"Terminal\" to do script [\"cd ${PWD}/../apps && ./SimElevatorServer_mac --port $port\"]"
+    osascript -e "tell app \"Terminal\" to do script [\"cd ${PWD}/.. && rebar3 shell --sname $node --apps elevator\"]"
+
+    #linux
+    gnome-terminal -x sh -c "cd ${PWD}/../apps; ./SimElevatorServer --port $port; exec bash"
+    gnome-terminal -x sh -c "cd ${PWD}/..; rebar3 shell --sname $node --apps elevator; exec bash"
+
+    port=$((port+1))
+done
