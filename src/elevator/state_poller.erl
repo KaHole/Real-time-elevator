8af1683efb9e940d4bb8d262bcf4fccf3be5750c
kahole <kristian@hole.priv.no>
2018-04-08 21:18:44 +0200

    doc

--- src/elevator/state_poller.erl
+++ src/elevator/state_poller.erl
@@ -2,14 +2,18 @@
 
 -export([start/1]).
 
-start(Pid) ->
-    register(state_poller, spawn(fun() -> poll_elevator_state(Pid, Elevator) end)).
+start(Pid, Elevator) ->
+    register(state_poller, spawn(fun() -> poller(Pid, Elevator) end)).
 
-poll_elevator_state(Pid, State) ->
+poller(Pid, State) ->
     % Polles new state and merges with existing
-    Polled_panel_state = get_floor_panel_state(Pid, [], length(State#elevator.cabRequests)-1),
+    CabCalls = get_cab_calls(Pid, [], length(State#elevator.cabRequests)-1),
+
+
+    % TODO: Ta et steg tilbake og design hele saken først
 
     % Get floor number. Ignores between_floor
+    % TODO: remove the duplicate request
     AtFloor = case elevator_interface:get_floor_sensor_state(Pid) of
         between_floors -> State#elevator.floor;
         _ -> elevator_interface:get_floor_sensor_state(Pid)
@@ -17,24 +21,24 @@ poll_elevator_state(Pid, State) ->
 
     _State = State#elevator{
         floor=AtFloor,
-        cabRequests=[A or B || {A,B} <- lists:zip(State#elevator.cabRequests, Polled_panel_state)]
+        cabRequests=[A or B || {A,B} <- lists:zip(State#elevator.cabRequests, CabCalls)]
     },
 
     receive
         {Sender, get_state} -> 
             Sender ! {updated_state, _State};
         {_, NewState} ->
-            elevator_state_poller(Pid, NewState#elevator{
+            poller(Pid, NewState#elevator{
                 floor=AtFloor,
                 cabRequests=[A or B || {A,B} <- lists:zip(NewState#elevator.cabRequests, Polled_panel_state)]
             })
     end,
-    elevator_state_poller(Pid, _State).
+    poller(Pid, _State).
 
-get_floor_panel_state(Pid, Floor_list, 0) ->
+get_cab_calls(Pid, Floor_list, 0) ->
     Floor_state = elevator_interface:get_order_button_state(Pid, 0, cab),
     [A == 1 || A <- lists:append([Floor_state], Floor_list)];
 
-get_floor_panel_state(Pid, Floor_list, Floor_number) -> 
+get_cab_calls(Pid, Floor_list, Floor_number) -> 
     Floor_state = elevator_interface:get_order_button_state(Pid, Floor_number, cab),
-    get_floor_panel_state(Pid, lists:append([Floor_state], Floor_list), Floor_number-1).
+    poll_cab_calls(Pid, lists:append([Floor_state], Floor_list), Floor_number-1).
