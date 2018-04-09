216cc0ebf7c233fe5fd844624f41b6fac33020fa
hola <havard.ola.eggen@gmail.com>
2018-04-09 17:32:23 +0200

    Added functionality for hall calls.

--- src/elevator/elevator_logic.erl
+++ src/elevator/elevator_logic.erl
@@ -82,12 +82,15 @@ elevator_controller(Pid) ->
     % Polled_panel_state = get_floor_panel_state(Pid, [], length(State#elevator.cabRequests)),
     elevator_state_poller ! {self(), get_state},
     receive
-        {updated_state, State} -> 
+        {updated_state, {State, HallCalls} -> 
+            % Handle hall calls as cab calls temporarily for determining direction of elevator
+            CabHallCall = [ Cab or Up or Down || {Cab,{Up,Down}} <- lists:zip(State#elevator.cabRequests, HallCalls)]
+            
             % Figure out which direction to go
-            _State = elevator_algorithm(State),
+            _State = elevator_algorithm(State, CabHallCall),
 
             % Check if arrive at a wanted floor
-            Checked_arrival_state = check_arrival(Pid, _State),
+            Checked_arrival_state = check_arrival(Pid, _State, CabHallCall, HallCalls),
 
             elevator_interface:set_motor_direction(Pid, Checked_arrival_state#elevator.direction),
             elevator_interface:set_floor_indicator(Pid, Checked_arrival_state#elevator.floor),
@@ -95,12 +98,19 @@ elevator_controller(Pid) ->
     end,
     elevator_controller(Pid).
 
-elevator_algorithm(State) ->
-    {Cab_request_down, Cab_request_up} = lists:split(State#elevator.floor+1, State#elevator.cabRequests),
+elevator_algorithm(State, CabHallCall) ->
+    % {Cab_request_down, Cab_request_up} = lists:split(State#elevator.floor+1, State#elevator.cabRequests),
+
+    % Go_up = lists:any(fun(X) -> X end, 
+    %     [lists:nth(State#elevator.floor+1, State#elevator.cabRequests)] ++ Cab_request_up
+    % ),
+
+    {Cab_request_down, Cab_request_up} = lists:split(State#elevator.floor+1, CabHallCall),
 
     Go_up = lists:any(fun(X) -> X end, 
-        [lists:nth(State#elevator.floor+1, State#elevator.cabRequests)] ++ Cab_request_up
+        [lists:nth(State#elevator.floor+1, CabHallCall)] ++ Cab_request_up
     ),
+
     Go_down = lists:any(fun(X) -> X end, Cab_request_down),
     Continue = case State#elevator.direction of
         up -> Go_up;
@@ -131,19 +141,24 @@ elevator_algorithm(State) ->
         _ -> State
     end.
 
-check_arrival(Pid, State) ->
+check_arrival(Pid, State, CabHallCall, HallCalls) ->
+    % StopAtFloor = lists:nth(
+    %     State#elevator.floor+1,
+    %     State#elevator.cabRequests
+    % ),
+
     StopAtFloor = lists:nth(
         State#elevator.floor+1,
-        State#elevator.cabRequests
+        CabHallCall
     ),
 
     % If we stop return new state, else return old
     case StopAtFloor of
-        true -> stop_at_floor(Pid, State);
+        true -> stop_at_floor(Pid, State, HallCalls);
         _ -> State
     end.
 
-stop_at_floor(Pid, State) ->
+stop_at_floor(Pid, State, HallCalls) ->
     elevator_interface:set_motor_direction(Pid, stop),
     elevator_interface:set_order_button_light(Pid, cab, State#elevator.floor, off),
     elevator_interface:set_door_open_light(Pid, on),
@@ -153,13 +168,32 @@ stop_at_floor(Pid, State) ->
         _ -> ok
     end,
     elevator_interface:set_door_open_light(Pid, off), % Close within 5 seconds?
-    State#elevator{
+    
+    _State = State#elevator{
         cabRequests=setnth(
             State#elevator.floor+1,
             State#elevator.cabRequests,
-            false
+            done
         )
-    }.
+    },
+    {Up, Down} = lists:nth(
+        State#elevator.floor+1,
+        HallCalls
+    ),
+    _HallCalls if 
+        Up and State#elevator.direction == up -> setnth(
+            State#elevator.floor+1,
+            HallCalls,
+            {done, Down}
+        );
+        Down and State#elevator.direction == down -> setnth(
+            State#elevator.floor+1,
+            HallCalls,
+            {Up, done}
+        );
+        _ -> ok
+    end,
+    {_State, _HallCalls}.
 
 % https://stackoverflow.com/questions/4776033/how-to-change-an-element-in-a-list-in-erlang
 setnth(1, [_|Rest], New) -> [New|Rest];
