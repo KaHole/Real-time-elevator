ba54751440911897ebc1de400c0c24c6f80a1792
kahole <kristian@hole.priv.no>
2018-04-10 13:12:33 +0200

    fix script

--- scripts/mac_4.sh
+++ scripts/mac_4.sh
@@ -9,9 +9,7 @@ do
     osascript -e "tell app \"Terminal\" to do script [\"cd ${PWD}/../apps && ./SimElevatorServer_mac --port $port\"]"
     osascript -e "tell app \"Terminal\" to do script [\"cd ${PWD}/.. && rebar3 shell --config config/$node.config  --sname $node --apps elevator\"]"
 
-    #linux
-    # gnome-terminal -x sh -c "cd ${PWD}/../apps; ./SimElevatorServer --port $port; exec bash"
-    # gnome-terminal -x sh -c "cd ${PWD}/..; rebar3 shell --sname $node --apps elevator; exec bash"
-
+    # kan være man bør starte alle simulatorene først
+    
     port=$((port+1))
 done
