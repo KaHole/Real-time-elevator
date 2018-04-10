5fea39a8a15874213ad2a69eb43dcb0596046790
kahole <kristian@hole.priv.no>
2018-04-10 14:12:20 +0200

    change from mac to linux

--- scripts/mac_4.sh
+++ scripts/mac_4.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# nodes=( one two three four )
+#nodes=( one two three four )
 nodes=( one )
 port=15657
 
@@ -10,6 +10,6 @@ do
     osascript -e "tell app \"Terminal\" to do script [\"cd ${PWD}/.. && rebar3 shell --config config/$node.config  --sname $node --apps elevator\"]"
 
     # kan være man bør starte alle simulatorene først
-    
+
     port=$((port+1))
 done
