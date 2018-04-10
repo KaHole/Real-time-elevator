bb5f49ec4672b9dc4f04b52118fce8ea2ae96900
kahole <kristian@hole.priv.no>
2018-04-10 13:11:13 +0200

    fix script

--- scripts/mac_4.sh
+++ scripts/mac_4.sh
@@ -7,7 +7,7 @@ port=15657
 for node in "${nodes[@]}"
 do
     osascript -e "tell app \"Terminal\" to do script [\"cd ${PWD}/../apps && ./SimElevatorServer_mac --port $port\"]"
-    osascript -e "tell app \"Terminal\" to do script [\"cd ${PWD}/.. && rebar3 shell --sname $node --apps elevator\"]"
+    osascript -e "tell app \"Terminal\" to do script [\"cd ${PWD}/.. && rebar3 shell --config config/$node.config  --sname $node --apps elevator\"]"
 
     #linux
     # gnome-terminal -x sh -c "cd ${PWD}/../apps; ./SimElevatorServer --port $port; exec bash"
