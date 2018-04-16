f3f9a3b8ba8a2fb3a7b6402116ff697d7c826596
hola <havard.ola.eggen@gmail.com>
2018-04-16 15:05:45 +0200

    Removed excess code.

--- src/network/discover.erl
+++ src/network/discover.erl
@@ -11,7 +11,6 @@ start() ->
     register(discover, spawn(fun() -> broadcast(Socket, nodes()) end)).
 
 broadcast(Socket, []) ->
-    %gen_udp:send(Socket, {172,20,10,15}, ?DISCOVER_PORT, atom_to_list(node(self()))),
     gen_udp:send(Socket, {129,241,187,255}, ?DISCOVER_PORT, atom_to_list(node(self()))),
     % gen_udp:send(Socket, {10,22,39,255}, ?DISCOVER_PORT, atom_to_list(node(self()))),
 
